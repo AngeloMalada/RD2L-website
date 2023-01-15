c00ba5cafd980727023e40845774379c180faf3f
Angelo Malada <angelo.malada@gmail.com>
2023-01-15 14:31:36 +0100

    fixed bug

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-  return (
-    <div className='h-[10vh]'>Footer</div>
-  )
-}
+  return <div className="h-[10vh]">Footerrrrr</div>;
+};
 
-export default Footer
+export default Footer;
